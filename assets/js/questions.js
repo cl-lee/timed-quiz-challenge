c954ec78140e532021296b39cf41fbed03d12192
cl.lee <cl.lee.ofz@gmail.com>
2023-01-12 16:22:08 +0000

    added question array into questions js file, added requirement comments to logic js file

---
+++ assets/js/questions.js
@@ -0,0 +1,12 @@
+let questions = [
+    {questionNumber: 1, question: "...?", answers: ["1.", "2.", "3.", "4."], correctAnswer: 1},
+    {questionNumber: 2, question: "...?", answers: ["1.", "2.", "3.", "4."], correctAnswer: 1},
+    {questionNumber: 3, question: "...?", answers: ["1.", "2.", "3.", "4."], correctAnswer: 1},
+    {questionNumber: 4, question: "...?", answers: ["1.", "2.", "3.", "4."], correctAnswer: 1},
+    {questionNumber: 5, question: "...?", answers: ["1.", "2.", "3.", "4."], correctAnswer: 1},
+    {questionNumber: 6, question: "...?", answers: ["1.", "2.", "3.", "4."], correctAnswer: 1},
+    {questionNumber: 7, question: "...?", answers: ["1.", "2.", "3.", "4."], correctAnswer: 1},
+    {questionNumber: 8, question: "...?", answers: ["1.", "2.", "3.", "4."], correctAnswer: 1},
+    {questionNumber: 9, question: "...?", answers: ["1.", "2.", "3.", "4."], correctAnswer: 1},
+    {questionNumber: 10, question: "...?", answers: ["1.", "2.", "3.", "4."], correctAnswer: 1},
+]
