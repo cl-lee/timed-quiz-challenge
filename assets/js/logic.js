956bde937ed25668098fc6bffd68eddc87e4c46b
cl.lee <cl.lee.ofz@gmail.com>
2023-01-13 18:38:32 +0000

    uncommented variable declarations in first few lines of logic.js

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,8 +1,8 @@
 // *define elements used in this page
-// let contentContainer = document.querySelector(".wrapper");
-// let startScreen = document.querySelector("#start-screen");
-// let questions = document.querySelector("#questions");
-// let endScreen = document.querySelector("#end-screen");
+let contentContainer = document.querySelector(".wrapper");
+let startScreen = document.querySelector("#start-screen");
+let questionsScreen = document.querySelector("#questions");
+let endScreen = document.querySelector("#end-screen");
 
 // - [ ]  A start button that when clicked a timer starts and the first question appears.
     
@@ -13,7 +13,7 @@
         // timerEL = document.getElementByID('time')
 
         // define time left 
-        // let timeLeft = 60;
+        // let timeLeft = 90;
     
         // timer: countdown function
         // let timeInterval = setInterval(function() {
@@ -34,18 +34,33 @@
             // questions.setAttribute("class", "");
 
             // function to loop through each question and answers
-                // for loop
+            
+            
+            //   - [ ] When answer is clicked, the next question appears
+            // if user has clicked on a correct answer, go to the next question
+            // addEventListener
+            
+            // map question to "#question-title" in index.html
+            // map choices to "#choices" in index.html
+            
+            // - [ ] Questions contain buttons for each answer
+            // display each choice as a button
+            // for loop
+            // for (let i = 0; i < questions.length; i++) {
+                
+                
+            // }
+            
 
 
 
 
-// - [ ] Questions contain buttons for each answer
 
 
 
 
 
-//   - [ ] When answer is clicked, the next question appears
+
 
 //   - [ ] If the answer clicked was incorrect then subtract time from the clock
             // if wrong answer, subtract (x) amount of time from timeLeft
@@ -64,13 +79,13 @@
 // questions and answers
 let questions = [
     {question: "question 1", choices: ["1.", "2.", "3.", "4."], correctAnswer: 1},
-    {question: "question 2?", choices: ["2-1.", "2.", "3.", "4."], correctAnswer: 0},
-    {question: "question 3?", choices: ["3-1.", "2.", "3.", "4."], correctAnswer: 3},
-    {question: "question 4?", choices: ["4-1.", "2.", "3.", "4."], correctAnswer: 1},
-    {question: "question 5?", choices: ["5-1.", "2.", "3.", "4."], correctAnswer: 2},
-    {question: "question 6?", choices: ["6-1.", "2.", "3.", "4."], correctAnswer: 1},
-    {question: "question 7?", choices: ["7-1.", "2.", "3.", "4."], correctAnswer: 0},
-    {question: "question 8?", choices: ["8-1.", "2.", "3.", "4."], correctAnswer: 2},
-    {question: "question 9?", choices: ["9-1.", "2.", "3.", "4."], correctAnswer: 1},
-    {question: "question 10?", choices: ["10-1.", "2.", "3.", "4."], correctAnswer: 3},
+    {question: "question 2", choices: ["2-1.", "2.", "3.", "4."], correctAnswer: 0},
+    {question: "question 3", choices: ["3-1.", "2.", "3.", "4."], correctAnswer: 3},
+    {question: "question 4", choices: ["4-1.", "2.", "3.", "4."], correctAnswer: 1},
+    {question: "question 5", choices: ["5-1.", "2.", "3.", "4."], correctAnswer: 2},
+    {question: "question 6", choices: ["6-1.", "2.", "3.", "4."], correctAnswer: 1},
+    {question: "question 7", choices: ["7-1.", "2.", "3.", "4."], correctAnswer: 0},
+    {question: "question 8", choices: ["8-1.", "2.", "3.", "4."], correctAnswer: 2},
+    {question: "question 9", choices: ["9-1.", "2.", "3.", "4."], correctAnswer: 1},
+    {question: "question 10", choices: ["10-1.", "2.", "3.", "4."], correctAnswer: 3},
 ]
