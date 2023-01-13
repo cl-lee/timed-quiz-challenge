d57d6f6709dd288cdc0d2a38c36e2d378acab7ed
cl.lee <cl.lee.ofz@gmail.com>
2023-01-13 16:11:36 +0000

    added more pseudocodes to logic.js file

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,3 +1,9 @@
+// *define elements used in this page
+// let contentContainer = document.querySelector(".wrapper");
+// let startScreen = document.querySelector("#start-screen");
+// let questions = document.querySelector("#questions");
+// let endScreen = document.querySelector("#end-screen");
+
 // - [ ]  A start button that when clicked a timer starts and the first question appears.
     
     // Event listener: click on "start quiz" button;
@@ -7,7 +13,7 @@
         // timerEL = document.getElementByID('time')
 
         // define time left 
-        // let timeLeft = 90;
+        // let timeLeft = 60;
     
         // timer: countdown function
         // let timeInterval = setInterval(function() {
@@ -21,17 +27,15 @@
         
         // 2) first question appears
 
-            // clear start-screen text
-
+            // clear start-screen
+            // startScreen.setAttribute("class", "hide");
             
-            // display id questions instead
-            
+            // display id: questions instead
+            // questions.setAttribute("class", "");
 
 // - [ ] Questions contain buttons for each answer
 
 
-
-// }, 1000);
 
 
 
@@ -39,7 +43,11 @@
 
 //   - [ ] If the answer clicked was incorrect then subtract time from the clock
             // if wrong answer, subtract (x) amount of time from timeLeft
+            // when timer reaches 0, clear timer
+            // clearInterval(timeInterval)
 
 // - [ ] The quiz should end when all questions are answered or the timer reaches 0.
 
 //  - [ ]  When the game ends, it should display their score and give the user the ability to save their initials and their score
+            // display end screen
+            // endScreen.setAttribute("class", "");
