5314170b1c7404fddaf0c22aecc3d73be4ddad82
cl.lee <cl.lee.ofz@gmail.com>
2023-01-14 22:17:38 +0000

    added codes for providing choices feedback, added timer for rendering choices feedback

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,15 +1,15 @@
 // questions and answers
 let questions = [
     {question: "question 1", choices: ["a1", "b", "c", "d"], correctAnswer: 1},
-    {question: "question 2", choices: ["a2", "b", "c", "d"], correctAnswer: 0},
-    {question: "question 3", choices: ["a3", "b", "c", "d"], correctAnswer: 3},
+    {question: "question 2", choices: ["a2", "b", "c", "d"], correctAnswer: 1},
+    {question: "question 3", choices: ["a3", "b", "c", "d"], correctAnswer: 1},
     {question: "question 4", choices: ["a4", "b", "c", "d"], correctAnswer: 1},
-    {question: "question 5", choices: ["a5", "b", "c", "d"], correctAnswer: 2},
+    {question: "question 5", choices: ["a5", "b", "c", "d"], correctAnswer: 1},
     {question: "question 6", choices: ["a6", "b", "c", "d"], correctAnswer: 1},
-    {question: "question 7", choices: ["a7", "b", "c", "d"], correctAnswer: 0},
-    {question: "question 8", choices: ["a8", "b", "c", "d"], correctAnswer: 2},
+    {question: "question 7", choices: ["a7", "b", "c", "d"], correctAnswer: 1},
+    {question: "question 8", choices: ["a8", "b", "c", "d"], correctAnswer: 1},
     {question: "question 9", choices: ["a9", "b", "c", "d"], correctAnswer: 1},
-    {question: "question 10", choices: ["a10", "b", "c", "d"], correctAnswer: 3},
+    {question: "question 10", choices: ["a10", "b", "c", "d"], correctAnswer: 1},
 ]
 
 // define variables used in this page
@@ -24,9 +24,9 @@ let questionsIndex = 0;
 
 // codes for user interaction
 // create an unordered list for containing the question choices
-let ul = document.createElement("ul");         
-choicesEL.appendChild(ul)
-let ulEL = document.querySelector("ul");
+let ol = document.createElement("ol");         
+choicesEL.appendChild(ol)
+let olEL = document.querySelector("ol");
 
 // Event listener for clicking on buttons;
 startScreen.addEventListener("click", function(event) {
@@ -55,7 +55,7 @@ startScreen.addEventListener("click", function(event) {
         let button = document.createElement("button");
         button.textContent = choice;
         button.setAttribute("data-index", i);
-        ul.appendChild(button);
+        ol.appendChild(button);
         }
     }
 })
@@ -67,16 +67,26 @@ function renderQuestions() {questionsScreen.addEventListener("click", function(e
     if(event.target.matches("button")) {
         questionsIndex++;
         document.querySelector("#question-title").textContent = questions[questionsIndex].question;
-        ulEL.innerHTML = "";
+        olEL.innerHTML = "";
+        feedbackEL.setAttribute("class", "hide")
 
         for (let i = 0; i < questions[questionsIndex].choices.length; i++) {
             choice = questions[questionsIndex].choices[i];
             let button = document.createElement("button");
             button.textContent = choice;
             button.setAttribute("data-index", i);
-            ul.appendChild(button);
+            ol.appendChild(button);
 
-            let userAnswer = event.target.getAttribute("data-index");
+            let userAnswer = parseInt(event.target.getAttribute("data-index"));
+            feedbackEL.setAttribute("class", "feedback")
+
+            if (userAnswer === questions[questionsIndex].correctAnswer) {
+              feedbackEL.innerHTML = "Correct!";
+            } else {
+              feedbackEL.innerHTML = "Wrong!"
+            }
+
+            feedbackTimer();
         }
     }
 })}  
@@ -88,6 +98,27 @@ function renderQuestions() {questionsScreen.addEventListener("click", function(e
 //  provide feedback = correct! or incorrect,
 //  unhide feedbackEL for 1.5s(?)
 //  hide feedbackEL after
+// function: render feedback
+function feedbackTimer() {
+    let feedbackTimeLeft = 1;
+    let feedbackTimeInterval = setInterval(function () {
+      //subtract from time left
+      feedbackTimeLeft--;
+  
+      //if statement where when we reach 0 time left,
+      if (feedbackTimeLeft === 0) {
+        
+        // we call display message
+        clearInterval(feedbackTimeInterval);
+        
+        // end the interval
+        feedbackEL.setAttribute("class", "hide");
+  
+      }
+  
+        },600);
+  }
+
 
 
 //  correct answer is worth 10 points
