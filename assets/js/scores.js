909cdbad729c4067ac5dd08e9a9561dda3d2d54a
cl.lee <cl.lee.ofz@gmail.com>
2023-01-16 15:51:53 +0000

    changed index.html page title, added clear highscores codes in score.js

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,5 +1,6 @@
+let clearHighScore = document.querySelector("#clear");
 
-
+// saves user initals and highscores into the local storage
 let highScores = JSON.parse(localStorage.getItem("highScores"))
 
 let initials = JSON.parse(localStorage.getItem("userInitials"));
@@ -11,3 +12,13 @@ highScores.push(highScoreEL);
 
 localStorage.setItem("highScores",JSON.stringify(highScores));
 
+// clear high scores
+clearHighScore.addEventListener("click", function(event) {
+    if(event.target.matches("button")) {
+        for (let i = 0; i < highScores.length; i++) {
+        highScores.pop();
+        }
+
+        localStorage.setItem("highScores",JSON.stringify(highScores));
+}
+})
