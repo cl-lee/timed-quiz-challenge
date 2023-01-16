374e965693c478ca99192ee436fc4eb6d5f374fb
cl.lee <cl.lee.ofz@gmail.com>
2023-01-16 15:37:03 +0000

    added codes to save user initials and scores to the highScores array in the local storage

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,6 +1,13 @@
-let highScore = [];
 
-let userInitials = JSON.parse(localStorage.getItem("userInitials"));
-let userScore = JSON.parse(localStorage.getItem("score"));
 
-highScore = highScore.push({})
+let highScores = JSON.parse(localStorage.getItem("highScores"))
+
+let initials = JSON.parse(localStorage.getItem("userInitials"));
+let score = JSON.parse(localStorage.getItem("score"));
+
+let highScoreEL = {userInitials: initials, userScore: score};
+
+highScores.push(highScoreEL);
+
+localStorage.setItem("highScores",JSON.stringify(highScores));
+
